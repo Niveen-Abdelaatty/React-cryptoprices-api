9334e95547c79d6de205c5cbc59b64ded7fd43a1
Niveen Abdelaatty <niveen.abdelaatty@gmail.com>
2023-03-21 13:27:21 -0400

    building an api

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -1,13 +1,28 @@
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import axios from 'axios';
 
 function Price() {
   const params = useParams();
-  
-  const apiKey = '452A8E3C-A1B5-4300-B4CB-A2C726563128';
+
+  const apiKey = '2FFA9750-D2ED-40B9-ACBD-D1C027E35041';
   const symbol = params.symbol;
-  
+
   const url = `http://rest-sandbox.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
-  console.log(url);
+
+  const getCoin = async () => {
+    try {
+      const res = await axios.get(url);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getCoin();
+  }, []);
 
   return <h1>Price</h1>;
 }
